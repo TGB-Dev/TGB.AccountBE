dce5aa7d1760ab7494a9edde8f7009c0404bfe58
Nguyen Hai Dang <nh.dang.643264@gmail.com>
2025-02-05 19:30:56 +0700

    hotfix(db): create trigger for updatedat field

--- TGB.AccountBE.API/Database/ApplicationDbInitializer.cs
+++ TGB.AccountBE.API/Database/ApplicationDbInitializer.cs
@@ -8,32 +8,35 @@ namespace TGB.AccountBE.API.Database;
 public class ApplicationDbInitializer : IHostedService
 {
     private readonly IConfiguration _configuration;
-    private readonly ApplicationDbContext _context;
-    private readonly RoleManager<IdentityRole> _roleManager;
-    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<ApplicationDbInitializer> _logger;
 
     public ApplicationDbInitializer(
         IConfiguration configuration,
-        ApplicationDbContext context,
-        RoleManager<IdentityRole> roleManager,
-        UserManager<ApplicationUser> userManager
-    )
+        IServiceProvider serviceProvider,
+        ILogger<ApplicationDbInitializer> logger
+        )
     {
         _configuration = configuration;
-        _context = context;
-        _roleManager = roleManager;
-        _userManager = userManager;
+        _serviceProvider = serviceProvider;
+        _logger = logger;
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
+        using var scope = _serviceProvider.CreateScope();
+        
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        
         // Seeding database
-        await RolesSeedAsync();
-        await UsersSeedAsync();
+        await RolesSeedAsync(roleManager);
+        await UsersSeedAsync(userManager);
 
         // Create trigger function and trigger for each table
-        await CreateTriggerFunctionAsync();
-        await CreateTriggerForTableAsync();
+        await CreateTriggerFunctionAsync(context);
+        await CreateTriggerForTableAsync(context);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
@@ -41,10 +44,9 @@ public class ApplicationDbInitializer : IHostedService
         return Task.CompletedTask;
     }
 
-    private async Task CreateTriggerFunctionAsync()
+    private async Task CreateTriggerFunctionAsync(ApplicationDbContext context)
     {
         const string createFunctionSql = """
-
                                          CREATE OR REPLACE FUNCTION set_updated_at()
                                          RETURNS TRIGGER AS $$
                                          BEGIN
@@ -53,38 +55,63 @@ public class ApplicationDbInitializer : IHostedService
                                          END;
                                          $$ LANGUAGE plpgsql;
                                          """;
-
-        await _context.Database.ExecuteSqlRawAsync(createFunctionSql);
+        try
+        {
+            await context.Database.ExecuteSqlRawAsync(createFunctionSql);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Failed to create trigger function");
+        }
     }
 
-    private async Task CreateTriggerForTableAsync()
+    private async Task CreateTriggerForTableAsync(ApplicationDbContext context)
     {
-        var tables = _context.Model.GetEntityTypes()
-            .Select(t => t.GetTableName())
-            .Distinct()
-            .ToList();
+        var entityTypes = context.Model.GetEntityTypes();
 
-        foreach (var sql in tables.Select(table => $"""
-                                                    DO $$
-                                                    BEGIN
-                                                        IF EXISTS (SELECT 1 
-                                                                   FROM pg_trigger t
-                                                                   JOIN pg_class c ON c.oid = t.tgrelid
-                                                                   WHERE c.relname = '{table}' 
-                                                                   AND t.tgname = 'set_updated_at_trigger') THEN
-                                                            EXECUTE 'DROP TRIGGER set_updated_at_trigger ON {table}';
-                                                        END IF;
-                                                    
-                                                        EXECUTE 'CREATE TRIGGER set_updated_at_trigger 
-                                                                 BEFORE UPDATE ON {table}
-                                                                 FOR EACH ROW 
-                                                                 EXECUTE PROCEDURE set_updated_at()';
-                                                    END $$;
-                                                    """))
-            await _context.Database.ExecuteSqlRawAsync(sql);
+        foreach (var entity in entityTypes)
+        {
+            if (entity.FindProperty("UpdatedAt") is null)
+            {
+                continue;
+            }
+            
+            var table = entity.GetTableName();
+            if (string.IsNullOrWhiteSpace(table))
+                continue;
+            
+            var sql = $"""
+                       DO $$
+                       BEGIN
+                           IF EXISTS (
+                               SELECT 1 
+                               FROM pg_trigger t
+                               JOIN pg_class c ON c.oid = t.tgrelid
+                               WHERE c.relname = '{table}' 
+                               AND t.tgname = 'set_updated_at_trigger'
+                           ) THEN
+                               EXECUTE 'DROP TRIGGER set_updated_at_trigger ON "{table}"';
+                           END IF;
+                           
+                           EXECUTE 'CREATE TRIGGER set_updated_at_trigger
+                                    BEFORE UPDATE ON "{table}"
+                                    FOR EACH ROW
+                                    EXECUTE PROCEDURE set_updated_at()';
+                       END
+                       $$ LANGUAGE plpgsql;
+                       """;
+            try
+            {
+                await context.Database.ExecuteSqlRawAsync(sql);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to create trigger for table {Table}", table);
+            }
+        }
     }
 
-    private async Task RolesSeedAsync()
+    private async Task RolesSeedAsync(RoleManager<IdentityRole> roleManager)
     {
         var roles = new List<IdentityRole>
         {
@@ -95,16 +122,18 @@ public class ApplicationDbInitializer : IHostedService
 
         foreach (var role in roles)
         {
-            if (role.Name is not null && await _roleManager.RoleExistsAsync(role.Name))
+            if (role.Name is not null && await roleManager.RoleExistsAsync(role.Name))
                 continue;
 
-            var roleResult = await _roleManager.CreateAsync(role);
-            if (!roleResult.Succeeded)
-                throw new Exception("Failed to create roles");
+            var roleResult = await roleManager.CreateAsync(role);
+            if (roleResult.Succeeded) continue;
+            
+            var errors = string.Join(", ", roleResult.Errors.Select(e => e.Description));
+            _logger.LogError($"Failed to create role {role.Name}: {errors}");
         }
     }
 
-    private async Task UsersSeedAsync()
+    private async Task UsersSeedAsync(UserManager<ApplicationUser> userManager)
     {
         var adminUser = new ApplicationUser
         {
@@ -116,15 +145,21 @@ public class ApplicationDbInitializer : IHostedService
 
         var adminPassword = _configuration["Admin:Password"]!;
 
-        if (await _userManager.FindByNameAsync(adminUser.UserName) is not null)
+        if (await userManager.FindByNameAsync(adminUser.UserName) is not null)
             return;
 
-        var userResult = await _userManager.CreateAsync(adminUser, adminPassword);
+        var userResult = await userManager.CreateAsync(adminUser, adminPassword);
         if (!userResult.Succeeded)
-            throw new Exception("Failed to create admin user");
+        {
+            var errors = string.Join(", ", userResult.Errors.Select(e => e.Description));
+            _logger.LogError($"Failed to create admin user: {errors}");
+        }
 
-        var roleResult = await _userManager.AddToRoleAsync(adminUser, Roles.Admin);
+        var roleResult = await userManager.AddToRoleAsync(adminUser, Roles.Admin);
         if (!roleResult.Succeeded)
-            throw new Exception("Failed to add admin role to admin user");
+        { 
+            var errors = string.Join(", ", roleResult.Errors.Select(e => e.Description));
+            _logger.LogError($"Failed to add role {Roles.Admin} to admin user: {errors}");
+        }
     }
 }
